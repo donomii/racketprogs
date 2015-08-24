83b0f5063d0d46558d766f4c7ab038aa92080759
donomii <donomii@gmail.com>
2015-08-25 05:14:42 +0900

    * Initial import

---
+++ Alfawiki/alfawiki.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+    "github.com/icza/gowut/gwu"
+    //"strconv"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
+	"fmt"
+	"os"
+)
+
+var  wikiText string
+
+type MyButtonHandler struct {
+    counter int
+    text    string
+}
+
+func (h *MyButtonHandler) HandleEvent(e gwu.Event) {
+    if _, isButton := e.Src().(gwu.Button); isButton {
+        ioutil.WriteFile(
+			"wikiFile.txt", 
+			[]byte(wikiText), os.FileMode(os.O_WRONLY|os.O_CREATE|os.O_TRUNC))
+    }
+}
+
+func main() {
+	
+	wikifile, err := ioutil.ReadFile("wikiFile.txt")
+	wikiText = string(wikifile)
+	if err!=nil { fmt.Printf("Couldn't load file: %v\n" , err)}
+    // Create and build a window
+    win := gwu.NewWindow("main", "Test GUI Window")
+    win.Style().SetFullWidth()
+    win.SetHAlign(gwu.HA_CENTER)
+    win.SetCellPadding(2)
+    
+    
+	   btn := gwu.NewButton("Save")
+    
+    win.Add(btn)
+    // ListBox examples
+    p := gwu.NewHorizontalPanel()
+    p.Style().SetBorder2(1, gwu.BRD_STYLE_SOLID, gwu.CLR_BLACK)
+    p.SetCellPadding(2)
+    
+    
+    // TextBox with echo
+    p = gwu.NewHorizontalPanel()
+	win.Add(p)
+    
+	q := gwu.NewHorizontalPanel()
+	win.Add(q)
+	
+	h := gwu.NewHtml("")
+	p.Add(h)
+	
+	
+    tb := gwu.NewTextBox(string(wikifile))
+	tb.SetRows(24)
+	tb.SetCols(80)
+    tb.AddSyncOnETypes(gwu.ETYPE_KEY_UP)
+	btn.AddEHandler(&MyButtonHandler{text: ":-)"}, gwu.ETYPE_CLICK)
+    q.Add(tb)
+	
+	
+    
+    tb.AddEHandlerFunc(func(e gwu.Event) {
+    wikiText = tb.Text()
+		h.SetHtml(string(blackfriday.MarkdownCommon([]byte(tb.Text()))))
+    
+		e.MarkDirty(h)
+    }, gwu.ETYPE_CHANGE, gwu.ETYPE_KEY_UP)
+    
+    
+	
+	
+	
+
+
+    // Create and start a GUI server (omitting error check)
+server := gwu.NewServer("guitest", "localhost:8081")
+    server.SetText("Test GUI App")
+    server.AddWin(win)
+	
+	server.Start("") // Also opens windows list in browser
+	
+	for {
+		
+	}
+    
+}
