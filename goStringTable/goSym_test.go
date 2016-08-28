bb76fe22e138f8c20ece8cbb7b0ac63c21bfcafe
Jeremy Price <donomii@gmail.com>
2016-08-28 03:37:41 +0200

    Finally working

---
+++ goStringTable/goSym_test.go
@@ -0,0 +1,11 @@
+package goSym
+
+import "testing"
+import "fmt"
+
+func TestSym(t *testing.T) {
+    s := New()
+    n := s.LookupOrCreate("Hello World")
+    fmt.Println(s)
+    fmt.Println("Lookup: ",n, s.GetString(n))
+}
