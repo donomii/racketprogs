85a44d7259260ade7397644095ddae92ce50c3df
Jeremy Price <donomii@gmail.com>
2018-10-20 17:01:46 +0200

    Reformat, fix bug

--- esmf/esmf.go
+++ esmf/esmf.go
@@ -1,24 +1,20 @@
-package main
+package esmf
 
 import "bytes"
 import "strings"
 import "errors"
 import "reflect"
-import "io"
 import "log"
-import "fmt"
 import "encoding/hex"
+import "fmt"
 
-type A struct {
-	Greeting string
-	Message  string
-}
+type byteString []byte
 
-func EncodeString (s string) string {
+func encodeString(s string) string {
 	l := len(s)
 	out := []string{}
-	for i:=0;i<l;i++ {
-		h := hex.EncodeToString([]byte(s[i:i+1]))
+	for i := 0; i < l; i++ {
+		h := hex.EncodeToString([]byte(s[i : i+1]))
 		out = append(out, h)
 	}
 	return strings.Join(out, " ")
@@ -33,8 +29,6 @@ func translate(obj interface{}, buff *bytes.Buffer) {
 	buff.WriteString(" $$")
 
 }
-
-var out io.Writer
 
 func translateRecursive(original reflect.Value, buff *bytes.Buffer) {
 	switch original.Kind() {
@@ -69,11 +63,13 @@ func translateRecursive(original reflect.Value, buff *bytes.Buffer) {
 		//log.Println("struct")
 		buff.WriteString("{{ ")
 		for i := 0; i < original.NumField(); i += 1 {
-			if i != 0 { buff.WriteString(" ,, ") }
+			if i != 0 {
+				buff.WriteString(" ,, ")
+			}
 			f := original.Field(i)
 			fname := original.Type().Field(i).Name
 			//log.Println(fmt.Sprintf("%v", original.Field(i).Name))
-			buff.WriteString(fmt.Sprintf("%v ,: ", EncodeString(fname)))
+			buff.WriteString(fmt.Sprintf("%v ,: ", encodeString(fname)))
 			//log.Println(EncodeString(fmt.Sprintf("%v", original.Field(i).Name)))
 			translateRecursive(f, buff)
 		}
@@ -82,7 +78,12 @@ func translateRecursive(original reflect.Value, buff *bytes.Buffer) {
 	// If it is a slice we create a new slice and translate each element
 	case reflect.Slice:
 		for i := 0; i < original.Len(); i += 1 {
+			if i != 0 {
+				buff.WriteString(" ,, ")
+			}
+
 			translateRecursive(original.Index(i), buff)
+
 		}
 
 	// If it is a map we create a new map and translate each value
@@ -98,14 +99,15 @@ func translateRecursive(original reflect.Value, buff *bytes.Buffer) {
 	// And everything else will simply be taken from the original
 	default:
 		//log.Println(fmt.Sprintf("%v", original))
-		buff.WriteString(EncodeString(fmt.Sprintf("%v", original)))
+		buff.WriteString(encodeString(fmt.Sprintf("%v", original)))
 	}
 }
 
-func toESMF (obj interface{}) string {
-var buff bytes.Buffer
-translate(obj, &buff)
-return string(buff.Bytes())
+//Given a go data structure, this will encode it to a string
+func ToESMF(obj interface{}) string {
+	var buff bytes.Buffer
+	translate(obj, &buff)
+	return string(buff.Bytes())
 }
 
 func decodeString(s string) (byte, error) {
@@ -117,15 +119,18 @@ func decodeString(s string) (byte, error) {
 	}
 }
 
-type byteString []byte
 func decodeBasic(s string) ([]byteString, error) {
 	codes := strings.Split(s, " ")
 	var out []byteString
-	if codes[0] != "^^" { return []byteString{}, errors.New("Missing ^^ at start of message, invalid format")}
-	if codes[len(codes)-1] != "$$" { return []byteString{}, errors.New("Missing $$ at start of message, invalid format")}
+	if codes[0] != "^^" {
+		return []byteString{}, errors.New("Missing ^^ at start of message, invalid format")
+	}
+	if codes[len(codes)-1] != "$$" {
+		return []byteString{}, errors.New("Missing $$ at start of message, invalid format")
+	}
 	for _, v := range codes {
 		b, err := decodeString(v)
-		if err != nil{
+		if err != nil {
 			//It might be a control code
 			out = append(out, []byte(v))
 		} else {
@@ -136,21 +141,23 @@ func decodeBasic(s string) ([]byteString, error) {
 	return out, nil
 }
 
-func joinByteStrings (b []byteString) byteString {
+func joinByteStrings(b []byteString) byteString {
 	var out byteString
-	for _,v := range  b {
+	for _, v := range b {
 		out = append(out, v...)
 	}
 	return out
 }
 
-func buildTokens(s []byteString) []byteString{
-	if  len(s) == 0 { return []byteString{} }
-	fmt.Println("Builtoken from: ", s[0])
-	if len(s[0]) ==1 {
-		i:=0
-		for i=0; i<len(s) && len(s[i]) ==1; i=i {
-			i=i+1
+func buildTokens(s []byteString) []byteString {
+	if len(s) == 0 {
+		return []byteString{}
+	}
+	//fmt.Println("Builtoken from: ", s[0])
+	if len(s[0]) == 1 {
+		i := 0
+		for i = 0; i < len(s) && len(s[i]) == 1; i = i {
+			i = i + 1
 		}
 		str := joinByteStrings(s[0:i])
 		return append([]byteString{str}, buildTokens(s[i:])...)
@@ -158,45 +165,50 @@ func buildTokens(s []byteString) []byteString{
 	return append([]byteString{s[0]}, buildTokens(s[1:])...)
 }
 
-func buildHash (b []byteString) (map[string]interface{}, []byteString) {
+func buildHash(b []byteString) (map[string]interface{}, []byteString) {
 	var h map[string]interface{} = map[string]interface{}{}
 	//Fucking unbelievable
 	first := string(b[0])
-	if first !=  "{{" {return h, b}
-	i:=0
-	for i=0; string(b[i]) != "}}"; i=i {
+	if first != "{{" {
+		return h, b
+	}
+	i := 0
+	for i = 0; string(b[i]) != "}}"; i = i {
 		key := b[i+1]
 		var val interface{}
 		val = b[i+3]
 		bval := b[i+1]
 		if string(bval) == "[[" || string(bval) == "{{" {
 			val, b = buildStruct(b[i+1:])
-			i=0
+			i = 0
 		} else {
-			i=i+4
+			i = i + 4
 		}
 		h[string(key)] = val
 	}
 	return h, b[i+1:]
 }
 
-
-func buildArray (b []byteString) (map[string]interface{}, []byteString) {
+func buildArray(b []byteString) (map[string]interface{}, []byteString) {
 	var h map[string]interface{} = map[string]interface{}{}
-	if string(b[0]) !=  "[[" {return h, b}
-	i:=0
-	for i=0; string(b[i]) != "]]"; i=i {
-		if i> 0 && string(b[i]) != ",," { panic ("Expected ,, got " + string(b[i])) }
+	if string(b[0]) != "[[" {
+		return h, b
+	}
+	i := 0
+	for i = 0; string(b[i]) != "]]"; i = i {
+		if i > 0 && string(b[i]) != ",," {
+			panic("Expected ,, got " + string(b[i]))
+		}
 		key := fmt.Sprintf("%v", i)
 		var val interface{}
 		val = b[i+1]
 		bval := b[i+1]
-		fmt.Println("bval is ", string(bval))
+		//fmt.Println("bval is ", string(bval))
 		if string(bval) == "[[" || string(bval) == "{{" {
 			val, b = buildStruct(b[i+1:])
 			i = 0
 		} else {
-			i=i+2
+			i = i + 2
 		}
 
 		h[key] = val
@@ -204,44 +216,32 @@ func buildArray (b []byteString) (map[string]interface{}, []byteString) {
 	return h, b[i+1:]
 }
 
-func fromESMF (s string) (map[string]interface{}, error){
-	b,_ := decodeBasic(s)
-	fmt.Println(b)
+//Given a string containing a message in esmf format, this will rebuild the Go data structure, and return it as a map[string]interface{}
+func FromESMF(s string) (map[string]interface{}, error) {
+	b, _ := decodeBasic(s)
+	//fmt.Println(b)
 	c := buildTokens(b)
-	fmt.Println(c)
-	for _,v := range c {
-		fmt.Println(string(v))
-	}
-	d,_ := buildStruct(c[2:])
-	fmt.Println(d)
+	//fmt.Println(c)
+	//for _, v := range c {
+	//	fmt.Println(string(v))
+	//}
+	d, _ := buildStruct(c[2:])
+	//fmt.Println(d)
 	return d, nil
 }
 
 func buildStruct(b []byteString) (map[string]interface{}, []byteString) {
-	c:= b[0]
+	c := b[0]
 	var v map[string]interface{}
-	fmt.Println("Building struct for: ", string(c))
-	if string(c)=="[[" {
-		fmt.Println("Building array")
+	//fmt.Println("Building struct for: ", string(c))
+	if string(c) == "[[" {
+		//fmt.Println("Building array")
 		v, b = buildArray(b)
-	} else{
-		if string(c)=="{{" {
-			fmt.Println("Building hash")
+	} else {
+		if string(c) == "{{" {
+			//fmt.Println("Building hash")
 			v, b = buildHash(b)
 		}
 	}
 	return v, b
-}
-
-func main () {
-	b := A{ "Hello", "World"}
-	fmt.Println(toESMF(b))
-	//message := "^^ 01 {{ 47 72 65 65 74 69 6e 67 :: 48 65 6c 6c 6f ,, 4d 65 73 73 61 67 65 :: 57 6f 72 6c 64 }} $$"
-	message := "^^ 01 [[ {{ 47 72 65 65 74 69 6e 67 :: 48 65 6c 6c 6f ,, 4d 65 73 73 61 67 65 :: 57 6f 72 6c 64 }} ,, {{ 47 72 65 65 74 69 6e 67 :: 48 65 6c 6c 6f ,, 4d 65 73 73 61 67 65 :: 57 6f 72 6c 64 }} ]] $$"
-	decoded, err := fromESMF(message)
-	if err == nil {
-		fmt.Println(decoded)
-	} else {
-		fmt.Println(err)
-	}
 }
