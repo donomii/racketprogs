b8345fc7f584c370db55bd3e2aee9c54d4128cb5
Donomii <donomii@gmail.com>
2019-04-02 20:23:58 +0000

    quon.c outputs ansi c, circular deletes target before compile

--- quonverter/circular.bash
+++ quonverter/circular.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 ./quon1 compiler.qon   > quon2.c
+rm quon2
 gcc -O3 quon2.c -o quon2 -Wno-unknown-escape-sequence
 ./quon2 --test
 #./quon_new compiler.qon
 ./quon2 compiler.qon > quon1.c
 diff quon1.c quon2.c
+rm quon1
 gcc -O3 quon1.c -o quon1 -Wno-unknown-escape-sequence
 bash circular.bash
