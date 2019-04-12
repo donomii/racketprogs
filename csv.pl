df19cbfc84b6bf4b760a35169d264f368c1ad190
Jeremy Price <jeremy.price@careem.com>
2019-04-12 19:41:28 +0200

    Load rows one by one to avoid OOM killer

--- csv.pl
+++ csv.pl
@@ -11,12 +11,15 @@ $tablename || die "Please provide tablename";
 my @rows;
 my $csv = Text::CSV->new ( { binary => 1 } ) or die "Cannot use CSV: ".Text::CSV->error_diag ();
 	 
-	my @columns = $csv->getline( *STDIN );
-	$csv->column_names(@columns);
+	my $columns = $csv->getline( *STDIN );
+	$csv->column_names(@$columns);
+    warn "Importing into table: $tablename, Importing columns: @$columns\n";
 	while ( my $row = $csv->getline_hr( *STDIN ) ) {
-		        push @rows, $row;
+        #push @rows, $row;
+        DataLib::AoH2Table($tablename, [$row]);
 		}
 		$csv->eof or $csv->error_diag();
 
-DataLib::AoH2Table($tablename, \@rows, "DROP");
+        warn "Loading rows into DB\n";
+        #DataLib::AoH2Table($tablename, \@rows, "DROP");
 DataLib::AoH2Table("ImportLog",[{Name=>$tablename, Url=>""}]);
