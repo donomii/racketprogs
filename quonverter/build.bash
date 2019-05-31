c343a35b77b5503a730fb60f548908e227bc6117
Jeremy Price <jeremy.price@careem.com>
2019-05-31 15:09:31 +0200

    Progress on java

--- quonverter/build.bash
+++ quonverter/build.bash
@@ -3,5 +3,9 @@
 gcc -O3   quon.c -o quon_new
 ./quon_new --test
 #./quon_new compiler.qon
-./quon_new compiler.qon --perl > test.pl
-perl test.pl --test
+./quon_new compiler.qon --java > test.java
+mkdir quonverter
+javac -d ./ test.java
+java -cp . quonverter/MyProgram
+jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
+java -jar MyProgram.jar
