fb46f3f67ff84f0205bed0ea426456efb245336d
Jeremy Price <jeremy.price@careem.com>
2019-05-31 15:51:30 +0200

    Java works?!?

--- quonverter/build.bash
+++ quonverter/build.bash
@@ -6,6 +6,6 @@ gcc -O3   quon.c -o quon_new
 ./quon_new compiler.qon --java > test.java
 mkdir quonverter
 javac -d ./ test.java
-java -cp . quonverter/MyProgram
+java -Xss100M -cp . quonverter/MyProgram
 jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
-java -jar MyProgram.jar
+java -Xss100M -jar MyProgram.jar
