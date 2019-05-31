8923d4f697f3322ccd6f8e10f3946dae5a0d9a88
Jeremy Price <jeremy.price@careem.com>
2019-05-31 16:29:17 +0200

    Fixes to make mandelbrot work with java

--- quonverter/build.bash
+++ quonverter/build.bash
@@ -3,9 +3,19 @@
 gcc -O3   quon.c -o quon_new
 ./quon_new --test
 #./quon_new compiler.qon
+
 ./quon_new compiler.qon --java > test.java
 mkdir quonverter
 javac -d ./ test.java
-java -Xss100M -cp . quonverter/MyProgram
+java -Xss100M -cp . quonverter/MyProgram --test
 jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
-java -Xss100M -jar MyProgram.jar
+java -Xss100M -jar MyProgram.jar --test
+
+
+./quon_new examples/mandelbrot.qon --java > test.java
+rm -r quonverter
+mkdir quonverter
+javac -d ./ test.java
+java -Xss100M -cp . quonverter/MyProgram --test
+jar -cvfm mandelbrot.jar MANIFEST.MF quonverter/*.class
+java -Xss100M -jar mandelbrot.jar --test
