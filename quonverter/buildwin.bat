5fb1746c61dfc770236d5c958bcf5e58ef0cb767
Jeremy Price <donomii@gmail.com>
2019-07-13 22:17:57 +0200

    Force compile for windows with -mwindows switch

--- quonverter/buildwin.bat
+++ quonverter/buildwin.bat
@@ -1,6 +1,6 @@
 quon compiler.qon   > quon.c
-gcc -O3 -Wl,--stack,67108864  quon.c -o quon_new
+gcc -O3 -mwindows -Wl,--stack,67108864  quon.c -o quon_new
 quon_new --test
-quon_new compiler.qon
+REM quon_new compiler.qon
 quon_new compiler.qon --perl > test.pl
 perl test.pl --test
