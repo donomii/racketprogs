f06eca46639c94d507fcf6d186141727ce945674
Jeremy Price <donomii@gmail.com>
2019-07-13 21:29:44 +0200

    Make build script work on windows

--- quonverter/buildwin.bat
+++ quonverter/buildwin.bat
@@ -1,7 +1,6 @@
-#!/bin/bash
-./quon compiler.qon   > quon.c
+quon compiler.qon   > quon.c
 gcc -O3 -Wl,--stack,67108864  quon.c -o quon_new
-./quon_new --test
-#./quon_new compiler.qon
-./quon_new compiler.qon --perl > test.pl
+quon_new --test
+quon_new compiler.qon
+quon_new compiler.qon --perl > test.pl
 perl test.pl --test
