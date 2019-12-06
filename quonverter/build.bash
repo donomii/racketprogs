e36442e298bcacc02bc398d9480f4081d3960dbd
Jeremy Price <jeremy.price@careem.com>
2019-12-06 22:13:37 +0100

    Build for lua

--- quonverter/build.bash
+++ quonverter/build.bash
@@ -1,23 +1,25 @@
 #!/bin/bash
 ./quon compiler.qon   > quon.c
-gcc -O3   quon.c -Wl,-stack_size,4000000 -o quon_new
-./quon_new --test
+gcc -Ofast -flto   quon.c -Wl,-stack_size,4000000 -o quon_new
+#./quon_new --test
 #./quon_new compiler.qon
 
 
 
-./quon_new compiler.qon --java > test.java
-mkdir quonverter
-javac -d ./ test.java
-java -Xss100M -cp . quonverter/MyProgram --test
-jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
-java -Xss100M -jar MyProgram.jar --test
+#./quon_new compiler.qon --java > test.java
+#mkdir quonverter
+#javac -d ./ test.java
+#java -Xss100M -cp . quonverter/MyProgram --test
+#jar -cvfm MyProgram.jar MANIFEST.MF quonverter/*.class
+#java -Xss100M -jar MyProgram.jar --test
 
 
-./quon_new examples/mandelbrot.qon --java > test.java
-rm -r quonverter
-mkdir quonverter
-javac -d ./ test.java
-java -Xss100M -cp . quonverter/MyProgram --test
-jar -cvfm mandelbrot.jar MANIFEST.MF quonverter/*.class
-java -Xss100M -jar mandelbrot.jar --test
+#./quon_new examples/mandelbrot.qon --java > test.java
+#rm -r quonverter
+#mkdir quonverter
+#javac -d ./ test.java
+#java -Xss100M -cp . quonverter/MyProgram --test
+#jar -cvfm mandelbrot.jar MANIFEST.MF quonverter/*.class
+#java -Xss100M -jar mandelbrot.jar --test
+
+./quon_new compiler.qon --lua
