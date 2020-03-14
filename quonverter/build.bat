08f0982aae88b8f4fe6e705ec30d36223b51dcb0
Jeremy Price <donomii@gmail.com>
2020-03-14 15:42:43 +0100

    Refactor, add new language

--- quonverter/build.bat
+++ quonverter/build.bat
@@ -1,15 +1,19 @@
-quon.exe compiler.qon   > quon.c
-gcc -O3 -Wl,--stack=99999999 quon.c -o quon_new
+mkdir working
+mkdir working\quonverter
+quon.exe compiler.qon   > working\quon.c
+gcc -O3 -Wl,--stack=99999999 working\quon.c -o quon_new
 quon_new.exe --test
 quon_new compiler.qon --perl  > test.pl
 perl test.pl --test
 quon_new compiler.qon --node  > test.js
 node --stack-size=6000 test.js --test
 mkdir quonverter
-quon_new compiler.qon --java  > quonverter\test.java
+quon_new compiler.qon --java  > working\quonverter\test.java
+cd working
 cd quonverter
 javac test.java
 cd ..
-jar cvfm quonverter.jar MANIFEST.MF quonverter\*.class
+cd ..
+jar cvfm quonverter.jar MANIFEST.MF working\quonverter\*.class
 java -Xmx1024M -Xss64M -jar quonverter.jar --test
 
