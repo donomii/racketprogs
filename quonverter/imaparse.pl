067e705d6411996486085311a728ea1ccbc908b0
Jeremy Price <donomii@gmail.com>
2020-03-29 13:45:53 +0200

    Remove empty brackets from declare

--- quonverter/imaparse.pl
+++ quonverter/imaparse.pl
@@ -15,19 +15,19 @@ my $grammar = '
 	structdef: structHeader typedefBody(s?) ")" ";" { print "))\n";1;}
 	identifier: /[A-Za-z_*][A-Za-z0-9_*]*/
 	atom: identifier { $return = $item[1] }| <perl_quotelike> { $return = join("", @{$item[1]}) }| /[0-9.-]*/ {$return=$item[1]}
-	expression: identifier "(" expression(s? /,/) ")" { $return =  "($item[1] @{$item[3]} )" } | atom { $return =  $item[1] }
-	return: "return" "(" expression ")" ";" { $return = "($item[1] $item[3])\n" }
-	expressionStatement: expression ";" { $return =  $item[1] }
-	ifStatement: "if" expression "then" statement(s?) "else" statement(s?) "end" { $return =  "( if $item[2] (then @{$item[4]}) (else @{$item[6]}))\n"}
-	setStatement: identifier "=" expression ";" { $return = "(set $item[1] $item[3])\n" }
-	nullReturn: "return" ";" { $return =  "(return)\n" }
+	expression: identifier "(" expression(s? /,/) ")" { $return =  "($item[1] @{$item[3]})" } | atom { $return =  $item[1] }
+	return: "return" "(" expression ")" ";" { $return = "\n($item[1] $item[3])" }
+	expressionStatement: expression ";" { $return =  "\n".$item[1] }
+	ifStatement: "if" expression "then" statement(s?) "else" statement(s?) "end" { $return =  "\n(if $item[2] \n(then @{$item[4]}) \n(else @{$item[6]}))\n"}
+	setStatement: identifier "=" expression ";" { $return = "\n(set $item[1] $item[3])" }
+	nullReturn: "return" ";" { $return =  "\n(return)" }
 	statement: ifStatement | setStatement | nullReturn | return | expressionStatement | <error>
 	fun: "fu" identifier "(" argList ")" "->" identifier declare statement(s)  "end" "function" { $return =  "($item[7] $item[2] (@{$item[4]})\n(declare $item[8])\n@{$item[9]})\n " } | <error>
 	functions: "Functions:" fun(s?) {print "@{$item[2]}" }
 	arg: identifier ":" identifier  { $return = "$item[3] $item[1]" }
 	argList: arg(s? /,/) { $return = $item[1] }
 	declare1: identifier ":" identifier expression { $return = "($item[1] $item[3] $item[4])\n" }
-	declare: declare1(s?) "in" { $return = "(@{$item[1]})\n" } | <error>
+	declare: declare1(s?) "in" { $return = scalar(@{$item[1]}) ? "(@{$item[1]})\n" : "" } | <error>
 	program: includes types functions | <error>
 ';
 # 
