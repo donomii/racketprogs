23e8be6d959b5bb8ef6b7d8b95b959b5712cbe1c
Jeremy Price <donomii@gmail.com>
2020-03-30 14:47:28 +0200

    Final tweaks, now works

--- quonverter/imaparse.pl
+++ quonverter/imaparse.pl
@@ -5,16 +5,16 @@
 	my $text = join(" ", <>);
 
 my $grammar = '
-	includes: "Includes:" {print "(includes)\n"} | <error>
-	typeHeader: "Types:" {print "(types\n"} | <error>
+	includes: "Includes:" atom(s)  ..."Types" {$return =  "(includes @{$item[2]})\n"} | <error>
+	typeHeader: "Types:"  | <error>
 	typeDecls: typedef|structdef
-	types: typeHeader typeDecls(s?) | <error>
-	typedefBody: identifier "is" identifier(s) ";" { print "($item[1] @{$item[3]})\n";1;}
-	typedef: "type" typedefBody
-	structHeader: "type" identifier "is" "struct" "("  {print "($item[2] (struct \n"}
-	structdef: structHeader typedefBody(s?) ")" ";" { print "))\n";1;}
+	types: typeHeader typeDecls(s?) {$return = "(types @{$item[2]})"}| <error>
+	typedefBody: identifier "is" identifier(s) ";" { $return =  "($item[1] @{$item[3]})\n";1;}
+	typedef: "type" typedefBody { $return = $item[2] }
+	structHeader: "type" identifier "is" "struct" "("  {$return =  $item[2]}
+	structdef: structHeader typedefBody(s?) ")" ";" {$return = "($item[1] (struct \n@{$item[2]}))"}
 	identifier: /[A-Za-z_*][A-Za-z0-9_*]*/
-	atom: identifier { $return = $item[1] }| <perl_quotelike> { $return = join("", @{$item[1]}) }| /[0-9.-]*/ {$return=$item[1]}
+	atom: ...!"Types" identifier { $return = $item[2] }| <perl_quotelike> { $return = join("", @{$item[1]}) }| /[0-9.-]*/ {$return=$item[1]}
 	expression: identifier "(" expression(s? /,/) ")" { $return =  "($item[1] @{$item[3]})" } | atom { $return =  $item[1] }
 	return: "return" "(" expression ")" ";" { $return = "\n($item[1] $item[3])" }
 	expressionStatement: expression ";" { $return =  "\n".$item[1] }
@@ -22,13 +22,13 @@ my $grammar = '
 	setStatement: identifier "=" expression ";" { $return = "\n(set $item[1] $item[3])" }
 	nullReturn: "return" ";" { $return =  "\n(return)" }
 	statement: ifStatement | setStatement | nullReturn | return | expressionStatement | <error>
-	fun: "fu" identifier "(" argList ")" "->" identifier declare statement(s)  "end" "function" { $return =  "($item[7] $item[2] (@{$item[4]})\n(declare $item[8])\n@{$item[9]})\n " } | <error>
-	functions: "Functions:" fun(s?) {print "@{$item[2]}" }
+	fun: "fu" identifier "(" argList ")" "->" identifier declare statement(s)  "end" "function" { $return =  "($item[7] $item[2] (@{$item[4]})\n(declare $item[8])\n(body @{$item[9]}))\n " } | <error>
+	functions: "Functions:" fun(s?) {$return =  "(functions \n\n@{$item[2]})" }
 	arg: identifier ":" identifier  { $return = "$item[3] $item[1]" }
 	argList: arg(s? /,/) { $return = $item[1] }
-	declare1: identifier ":" identifier expression { $return = "($item[1] $item[3] $item[4])\n" }
-	declare: declare1(s?) "in" { $return = scalar(@{$item[1]}) ? "(@{$item[1]})\n" : "" } | <error>
-	program: includes types functions | <error>
+	declare1: identifier ":" identifier expression { $return = "\n($item[3] $item[1] $item[4])" }
+	declare: declare1(s?) "in" { $return = scalar(@{$item[1]}) ? "@{$item[1]}" : "" } | <error>
+	program: includes types functions {$return = "(\n$item[1]\n$item[2]\n$item[3]\n)"}| <error>
 ';
 # 
      # Generate a parser from the specification in $grammar:
@@ -40,4 +40,5 @@ my $grammar = '
      # defined in $grammar):
 
      use Data::Dumper;
-        print Dumper($parser->program($text));
+        my $p = $parser->program($text);
+        print $p;
