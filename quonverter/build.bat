6e5b787933da5a6d2665ff97629b3b87ecb9aaa8
Jeremy Price <donomii@gmail.com>
2020-05-23 21:32:25 +0200

    Use work directory

--- quonverter/build.bat
+++ quonverter/build.bat
@@ -1,19 +1,19 @@
-mkdir working
-mkdir working\quonverter
-quon.exe compiler.qon   > working\quon.c
-gcc -O3 -Wl,--stack=99999999 working\quon.c -o quon_new
+mkdir work
+mkdir work\quonverter
+quon.exe compiler.qon   > work\quon.c
+gcc -O3 -Wl,--stack=99999999 work\quon.c -o quon_new
 quon_new.exe --test
-quon_new compiler.qon --perl  > test.pl
+quon_new compiler.qon --perl  > work\test.pl
 perl test.pl --test
-quon_new compiler.qon --node  > test.js
+quon_new compiler.qon --node  > work\test.js
 node --stack-size=6000 test.js --test
 mkdir quonverter
-quon_new compiler.qon --java  > working\quonverter\test.java
-cd working
+quon_new compiler.qon --java  > work\quonverter\test.java
+cd work
 cd quonverter
 javac test.java
 cd ..
 cd ..
-jar cvfm quonverter.jar MANIFEST.MF working\quonverter\*.class
+jar cvfm quonverter.jar MANIFEST.MF work\quonverter\*.class
 java -Xmx1024M -Xss64M -jar quonverter.jar --test
 
