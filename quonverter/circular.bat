0ca009924b5227df9662e30f323a1e07b28880f0
Your Name <you@example.com>
2020-08-18 01:31:41 +0200

    Progress on ansi2

--- quonverter/circular.bat
+++ quonverter/circular.bat
@@ -1,11 +1,9 @@
-#!/bin/bash
 cp quon.exe quon1.exe
 echo Preparing quon2.c
-quon1 compiler.qon   > quon1.c
-rm quon2
+quon1.exe compiler.qon   > quon1.c
 rm quon2.exe
 echo Compiling quon2.c into quon2
-gcc -O3 -Wl,--stack=999999999 quon1.c -o quon2 -Wno-unknown-escape-sequence
+gcc -O3 -Wl,--stack=999999999 quon1.c -o quon2.exe -Wno-unknown-escape-sequence
 echo Testing quon2
 quon2 --test
 echo Preparing quon1.c using quon2
@@ -15,5 +13,4 @@ quon2 compiler.qon > quon2.c
 echo Removing quon1
 rm quon1
 echo Compiling quon1.c into quon1
-gcc -O3 -Wl,--stack=999999999 quon2.c -o quon1 -Wno-unknown-escape-sequence
-#bash circular.bash
+gcc -O3 -Wl,--stack=999999999 quon2.c -o quon1.exe -Wno-unknown-escape-sequence
