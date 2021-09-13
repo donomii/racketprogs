a4e72a217c9ee1846c1b0cb340fe4169f881b609
Jeremy Price <jeremy.price@grafana.com>
2021-09-13 21:49:15 +0200

    .

--- pmoolib/main.go
+++ pmoolib/main.go
@@ -118,7 +118,7 @@ func SaveObject(o *Object) {
 }
 
 func LoadObject(id string) *Object {
-	n_id := strconv.Atoi(id)
+	n_id, _ := strconv.Atoi(id)
 	id = ToStr(n_id)
 	//log.Println("Loading " + "objects/" + id + ".json")
 	file, err := ioutil.ReadFile("objects/" + id + ".json")
