dea60efe0e7a398c060423a103b85850341dd4ac
Jeremy Price <jeremy.price@grafana.com>
2021-09-17 21:29:34 +0200

    .

--- queue/main.go
+++ queue/main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
+	"github.com/philippgille/gokv/badgerdb"
 )
 
 func main() {
@@ -16,5 +17,57 @@ func main() {
 		data, _ := ioutil.ReadAll(c.Request.Body)
 		Q <- data
 	})
+
+	r.POST("/store/:key", func(c *gin.Context) {
+		data, _ := ioutil.ReadAll(c.Request.Body)
+		key := c.Param("key")
+		StoreObject(key, data)
+	})
+
+	r.GET("/fetch/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		c.Writer.Write(LoadObject(key))
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+var KVstore badgerdb.Store
+var err error
+
+func StartKVstore() {
+	options := badgerdb.DefaultOptions // Address: "localhost:6379", Password: "", DB: 0
+
+	// Create client
+	KVstore, err = badgerdb.NewStore(options)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func StoreObject(id string, m []byte) {
+	err := KVstore.Set(id, m)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LoadObject(id string) []byte {
+	retrievedVal := new([]byte)
+	found, err := KVstore.Get(id, retrievedVal)
+	if err != nil {
+		panic(err)
+	}
+	if !found {
+		panic("Value not found")
+	}
+	return *retrievedVal
+}
+
+func DeleteObject(id string) {
+	err := KVstore.Delete(id)
+	if err != nil {
+		panic(err)
+	}
 }
