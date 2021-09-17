b6c0062fc23d1bb7e1df41924543b9c1880e4ef6
Jeremy Price <jeremy.price@grafana.com>
2021-09-17 20:39:02 +0200

    .

---
+++ pmoolib/myQ.go
@@ -0,0 +1,58 @@
+package myQ
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+
+	"net/http"
+
+	"github.com/donomii/pmoo"
+)
+
+func Receiver(url string, callback func([]byte)) {
+	for {
+		resp, err := http.Get(url + "/subscribe/main")
+		if err != nil {
+			//log.Fatalln(err)
+		} else {
+			data, _ := ioutil.ReadAll(resp.Body)
+			callback(data)
+		}
+	}
+}
+
+func Message(url string, mess interface{}) {
+	json, _ := json.Marshal(mess)
+	Send(url, json)
+}
+
+func Send(url string, data []byte) {
+	http.Post(url+"/publish/main", "who/cares", bytes.NewReader(data))
+}
+
+func StoreObject(url, id string, m pmoo.Object) {
+	b, _ := json.Marshal(m)
+	http.Post(url+"/store/"+id, "who/cares", bytes.NewReader(b))
+}
+
+func LoadObject(url, id string) *pmoo.Object {
+	resp, err := http.Get(url + "/subscribe/main")
+	if err != nil {
+		//log.Fatalln(err)
+	}
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	retrievedVal := new(pmoo.Object)
+	json.Unmarshal(data, retrievedVal)
+	return retrievedVal
+}
+
+/*
+func DeleteObject(id string) {
+	err := KVstore.Delete(id)
+	if err != nil {
+		panic(err)
+	}
+}
+*/
