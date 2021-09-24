30b358d5550c5b82f2972082e17eb185378797f8
Jeremy Price <jeremy.price@grafana.com>
2021-09-24 16:40:01 +0200

    .

--- pmoolib/main.go
+++ pmoolib/main.go
@@ -605,7 +605,7 @@ func NameSearch(o *Object, aName string) (*Object, *Property) {
 
 func ParseDo(s string, playerId string) (string, string) {
 	s = strings.Trim(s, " \n\r")
-	log.Printf("Parsing '%v'\n", []byte(s))
+	log.Printf("Parsing '%v', len: %v\n", []byte(s), len(s))
 	if playerId == "" {
 		return "", ""
 	}
