d3005b63c394fbcca6e67c8af28524dc59ccd052
Jeremy Price <jeremy.price@grafana.com>
2021-09-26 13:44:17 +0200

    Layouts now kind of working

--- lastgui/lastgui.rkt
+++ lastgui/lastgui.rkt
@@ -6,11 +6,17 @@
 
 [define m '[w "toplevel" [id "Toplevel container"] [type "toplevel"]
               [children
-               [w "A Test Window" [id "Test window"] [type "window"] [x 50] [y 50]
+               [w "A Test Window" [id "Test window"] [type "window"] [x 50] [y 50] [advancer window]
                   [children
-                   [w "A handy little paragraph of text that should test the renderer a bit"
-                      [id "test text"] [type "text"]]
-                   [w "OK" [id "ok button"] [type "button"]]]]
+                        [w "A big container" [type "container"] [advancer vertical] [children
+                                                                                                            [w "A h1container" [type "container"] [advancer vertical][children
+                                                                                                                                                 [w "A handy little paragraph of text that should test the renderer a bit"
+                                                                                                                                                    [id "test text"] [type "text"][advancer horizontal]]
+                                                                                                                                                 [w "OK" [id "ok button"] [type "button"][advancer horizontal]]]]
+                                                                                                            [w "A h2container" [type "container"] [advancer horizontal][children
+                                                                                                                                                 [w "A handy little paragraph of text that should test the renderer a bit"
+                                                                                                                                                    [id "test text"] [type "text"]]
+                                                                                                                                                 [w "OK" [id "ok button"] [type "button"]]]]]]] ]
                [w "Another Test Window" [id "Another Test window"] [type "window"] [x 150] [y 150]
                   [children
                    [w "A handy little paragraph of text that should test the renderer a bit"
@@ -19,12 +25,16 @@
                                                         
               [w "menu" [id "Popup menu"] [type "popup"][children
                                                          [w "Do thing" [type "button"] [id "do thing button"]]
-                                                         [w "Exit" [id "exit button"][type "button"]]]]
+                                                         [w "Exit" [id "exit button"][type "button"]]]]]
              
-              ]]
+              ]
+
+
 
 [define [mx x] [s= mx x]]
 [define [my x] [s= my x]]
+[define [nx x] [s= nextx x]]
+[define [ny x] [s= nexty x]]
 [define [startx x] [s= startx x]]
 [define [starty x] [s= starty x]]
 [define [mouse-event x] [s= mouse-event x]]
@@ -32,7 +42,18 @@
 [define [button-down? x] [s= button-down?  x]]
 [define [vertical-advancer x y x2 y2] [list x y2]]
 [define [horizontal-advancer x y x2 y2] [list x2 y]]
-
+[define [new-advancer name bounds state]
+  [when name
+      [printf "Advancer: ~a Bounds: ~a~n" name bounds]]
+   [let [[res [cond
+      [[equal? name 'window][set-state 'nextx [first bounds] [set-state 'nexty [+ 22 [second bounds]] state]]]
+      [[equal? name 'horizontal][set-state 'nextx [third bounds] [set-state 'nexty [second bounds] state]]]
+      [[equal? name 'vertical][set-state 'nextx [first bounds] [set-state 'nexty [fourth bounds] state]]]
+      [else state]
+      
+       ]]]
+     ;[printf "Advanced: ~a~n" res]
+     res]]
 
 [define last-state `[
                      [mx . 0]
@@ -92,11 +113,11 @@
       [set-state 'drag-target id state]
       state]]
 
-[define [render data type attribs children t state]
+[define [render data type attribs children t state parent-bounds]
   ;[printf "Rendering ~a~nState: ~a~n" type state]
 
-  [letrec [[x [mx state]]
-           [y  [my state]]
+  [letrec [[x [nx state]]
+           [y  [ny state]]
            [id [s=f id attribs #f]]
            [advancer [s=f advancer state #f]]]
     
@@ -115,9 +136,9 @@
                              [text-size 11]
                              [fill 0]
                              [text data x y 100 100 ]
-                             [list attribs
+                             [list [list x y x2 y2] attribs
                                    [set-drag-target-if [and hover? [equal? [mouse-event state] 'press]] id
-                                                       [set-state 'mx [car nextPos] [set-state 'my [cadr nextPos] state] ]]]]]
+                                                       [set-state 'nextx [car nextPos] [set-state 'nexty [cadr nextPos] state] ]]]]]
       [[equal? type "button"] [letrec [
                                        [x2 [+ x 50]]
                                        [y2 [ + y 15]]
@@ -135,16 +156,16 @@
                                   [fill 0]
                                   [text data x y ]
                                   ]
-                                 [printf "Hover:~a id:~a mouse-event:~a drag-target:~a is drag-target:~a\n" hover? [s= id attribs] [mouse-event state] [s= drag-target state][equal? [s= drag-target state] [s= id attribs]]]
+                                 ;[printf "Hover:~a id:~a mouse-event:~a drag-target:~a is drag-target:~a\n" hover? [s= id attribs] [mouse-event state] [s= drag-target state][equal? [s= drag-target state] [s= id attribs]]]
                                 [when hover?
                                   
                                     [when [equal? [s= drag-target state] [s= id attribs]]
                                     [when [equal? [mouse-event state] 'release]
                                       [button-click  [s= id attribs]]
                                       ]]]
-                                [list attribs
+                                [list [list x y x2 y2] attribs
                                       [set-drag-target-if [and hover? [equal? [mouse-event state] 'press]] id
-                                                          [set-state 'mx [car nextPos] [set-state 'my [cadr nextPos] state ] ]]]]]
+                                                          [set-state 'nextx [car nextPos] [set-state 'nexty [cadr nextPos] state ] ]]]]]
       [[equal? type "window"]
        ;[printf "case: window~n"]
        [letrec [
@@ -178,19 +199,35 @@
                ]]]
          
          [list
+          [list x y x2 y2]
           [set-attrib 'y y [set-attrib 'x x attribs]]
           [set-drag-target-if [and hover? [equal? [mouse-event state] 'press]] id
-                              [set-state 'mx  [car [advancer x y x2  [+ y 22]]]
-                                         [set-state 'my  [cadr [advancer x y x2  [+ y 22]]] state]] ]]]]
+                              [set-state 'nextx  [car [advancer x y x2  [+ y 22]]]
+                                         [set-state 'nexty  [cadr [advancer x y x2  [+ y 22]]] state]] ]]]]
 
 
       
       [[equal? type "popup"] [begin
-                               [list attribs
-                                     [set-state 'mx [startx state]
-                                                [set-state 'my [starty state] state]]]
+                               [list [list x y x y] attribs
+                                     [set-state 'nextx [startx state]
+                                                [set-state 'nexty [starty state] state]]]
                                ]]
-      [else [list attribs state]]
+      [[equal? type "container"][letrec [
+                                    [x2 [+ x 100]]
+                                    [y2 [ + y 100]]
+                                    [nextPos  [list x y]]
+                                    [hover? [inside? mouse-x mouse-y x y x2 y2]]
+                                    ]
+                             [fill 255]
+                             [stroke 0 0 0 255]
+                             [rect x y  100 100]
+                             [text-size 11]
+                             [fill 0]
+                             [text data x y 100 100 ]
+                             [list [list x y x2 y2] attribs
+                                   
+                                                       [set-state 'nextx [car nextPos] [set-state 'nexty [cadr nextPos] state] ]]]]
+      [else [list parent-bounds attribs state]]
     
   
       ]
@@ -199,24 +236,34 @@
 
   ]
 
-
-
-[define [map-children children state]
+;find the smallest box that covers both bounding boxes
+[define [merge-bounds b1 b2]
+[list
+[if [> [first b1] [first b2]] [first b1] [first b2]]
+[if [> [second b1] [second b2]] [second b1] [second b2]]
+[if [> [third b1] [third b2]] [third b1] [third b2]]
+[if [> [fourth b1] [fourth b2]] [fourth b1] [fourth b2]]
+ ]
+  ]
+[define [map-children children state parent-bounds]
   ;[printf "Child list: ~a~n" children]
+  [let [[out-bounds parent-bounds]]
   [let [[returns  [map [lambda [c]
                          ;[printf "Mapping ~a~n" [second c]]
-                         [letrec [[alist [parse-tree c state]]
-                                  [template [car alist]]
-                                  [new-state [cadr alist]]]
+                         [letrec [[alist [parse-tree c state parent-bounds]]
+                                  [new-widget [car alist]]
+                                  [new-state [cadr alist]]
+                                  [new-bounds [caddr alist]]]
+                           [set! new-bounds [merge-bounds new-bounds out-bounds]]
                            [set! state new-state]
-                           template
+                           new-widget
                            ]] children]]]
     ;[printf "Returning children: ~a~n" returns]
     [list returns
-          state]
-    ]]
+          state out-bounds]
+    ]]]
 
-[define [parse-tree t state]
+[define [parse-tree t state parent-bounds]
   ;[printf "Parsetree ~a~nState: ~a~n~n" t state]
   [letrec [
            [ data [cadr t]]
@@ -227,27 +274,40 @@
     ;Used to enable/disable widgets
     [if [want-to-render? t state]
         ;render sub tree
-        [letrec [[alist [render data type attribs children t state]]
-                 [new-attribs [car alist]]
-                 [new-state [cadr alist]]
+        [letrec [[alist [render data type attribs children t state parent-bounds]]
+                 [new-parent-bounds [car alist]]
+                 [new-attribs [cadr alist]]
+                 [advancer [s= advancer state]]
+                 [nextPos  [apply advancer new-parent-bounds]]
+                 [new-state [set-state 'mx [car nextPos] [set-state 'my [cadr nextPos] [caddr alist]]]]
                  [children1 [assoc 'children new-attribs]]
                  [new-widget   [append `[w ,data ]  new-attribs]]
+                 [old-advancer [s= advancer state]]
                  ]
+          ;[when  [s=f advancer attribs #f]
+            
+              ;[set! new-state [new-advancer [car [s= advancer attribs]] new-parent-bounds new-state]]]
           ;[printf "Parsetree1 ~a~nState: ~a~n~n" new-widget new-state]
           ;If there are children
           [if children1
               ;Render them
-              [letrec [[new-alist [map-children [cdr children1] new-state]]
+              [letrec [[new-alist [map-children [cdr children1] new-state new-parent-bounds]]
                        [new-children [car new-alist]]
                        [new-state1 [cadr new-alist]]
+                       [new-child-bounds [caddr new-alist]]
                        [new-new-attribs [delete-duplicates [cons [cons 'children new-children]   new-attribs] [lambda [x y] [equal? [car x] [car y]]]]]
                        [new-new-widget   [append `[w ,data ]  new-new-attribs]]]
                 ;[printf "~nNew widget ~a~nNew state: ~a~nNew children ~a~n~n" new-new-widget new-state1 new-children]
       
-          
-                [list new-new-widget new-state1]]
-              [list t new-state]]] ;FIXME use new attributes
-        [list t state] ;FIXME process mouse events without render
+         
+            
+              [set! new-state1 [new-advancer [car [s=f advancer attribs '[#f #f]]] new-child-bounds new-state1]]
+                [list new-new-widget new-state1 new-child-bounds]]
+              [begin
+                [set! new-state [new-advancer [car [s=f advancer attribs '[#f #f]]] new-parent-bounds new-state]]
+                [list t new-state new-parent-bounds]
+                ]]] ;FIXME use new attributes
+        [list t state [list 0 0 0 0]] ;FIXME process mouse events without render
         ]]]
 
 
@@ -268,7 +328,8 @@
   ;[printf "Last state: ~a~n" last-state]
   ;[printf "Last template ~a~n" m]
 
-  [letrec [[alist [parse-tree  m `[
+  [letrec [[alist [parse-tree  m `[[nextx . 0]
+                                   [nexty . 0]
                                    [mx . ,mouse-x]
                                    [my . ,mouse-y]  ;Current draw position
                                    [startx  .  ;Drag start x
@@ -288,7 +349,8 @@
                                    [drag-target . ,[s= drag-target last-state]]
                                    [dragvecx . ,[- mouse-x [startx last-state]]]
                                    [dragvecy . ,[- mouse-y [starty last-state]]] ;Total drag vector, x and y
-                                   ]]]
+                                   ]
+                               [list 0 0 0 0]]]
            [new-state [cadr alist]]
            [new-template [car alist]]
            ]
